75beb20efb812934660cf4d0da948dd79007a73f
leonardo <ivanpachecoleo23@gmail.com>
2025-03-12 23:41:36 -0500

    add new models, Coments, Category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        self.nombre
+    
+    class Meta:
+        verbose_name_plural = 'Categorias'
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+
